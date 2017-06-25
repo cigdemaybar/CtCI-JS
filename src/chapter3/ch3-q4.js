e1abe95188be5a7c6b491b4b88458715b44db4b2
Cigdem Aybar <aybarcigdem@gmail.com>
2017-06-24 23:11:52 -0400

    ch3-q4 solved

--- src/chapter3/ch3-q4.js
+++ src/chapter3/ch3-q4.js
@@ -1,31 +1,18 @@
 'use strict';
 
-/**
- * Queues and Stacks have different orders for extracting items. To create a
- * queue with stacks we have two stacks, one for inserting items and one for
- * extracting them. When dequeuing an item if the extract stack is empty we
- * use queue operations to pop all the items off the insert stack onto the
- * extract stack which will now be in the right order for a queue.
- *
- * N = |MyQueue|
- * Time: enqueue O(1), dequeue O(N)
- * Additional space: O(N) - to hold the input items
- */
-export class MyQueue {
+export class Queue {
   constructor() {
     this.eStack = [];
     this.dStack = [];
   }
-
-  enqueue(value) {
-    this.eStack.push(value);
+  enqueue(val) {
+    this.eStack.push(val);
   }
-
   dequeue() {
-    if (this.dStack.length === 0 && this.eStack.length === 0) {
+    if (!this.dStack.length && !this.eStack.length) {
       throw new Error('queue is empty');
     }
-    if (this.dStack.length === 0) {
+    if (!this.dStack.length) {
       while (this.eStack.length > 0) {
         this.dStack.push(this.eStack.pop());
       }
