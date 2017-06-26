9f47826bcc55bc71cc6fa0c1ad40b313f20f022d
Cigdem Aybar <aybarcigdem@gmail.com>
2017-06-26 18:06:26 -0400

    davis staircase solved with memoization

--- src/hackerrank/recursionDavisStaircase.js
+++ src/hackerrank/recursionDavisStaircase.js
@@ -9,3 +9,15 @@ function steps(num) {
     return 4;
   }
 }
+
+// OR memoization
+
+function steps(num) {
+  if (num < 0) {
+    return 0;
+  } else if (num === 0) {
+    return 1;
+  } else if (num > 0) {
+    return steps[num] ? steps[num] : steps[num] = steps(num - 1) + steps(num - 2) + steps(num - 3);
+  }
+}
