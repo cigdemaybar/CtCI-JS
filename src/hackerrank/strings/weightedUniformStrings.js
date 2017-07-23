780df3bdee349961e0ff4b6fc97efb0d20ae5e31
Cigdem Aybar <aybarcigdem@gmail.com>
2017-07-23 18:25:16 -0400

    weightedUniformStrings solution is added

--- src/hackerrank/strings/weightedUniformStrings.js
+++ src/hackerrank/strings/weightedUniformStrings.js
@@ -1,3 +1,15 @@
+function inc(hash, n, s){
+    for(var key in hash){
+        var ind = s.indexOf(key)
+        var val = s.charCodeAt(ind) - 96
+        if(n % val === 0){
+            if (val*hash[key] >= n){
+                return ('Yes')
+            }
+        }
+    }
+    return ('No')
+}
 function main() {
     var s = readLine();
     var n = parseInt(readLine());
@@ -5,19 +17,23 @@ function main() {
     for(var i=0; i < s.length; i++){
         (hash[s[i]]) ? hash[s[i]]++ : hash[s[i]] = 1;
     }
-    var vals = new Set();
+    //var hash2 = {}
     for(var key in hash){
-        var ind = s.indexOf(key)
-        var val = s.charCodeAt(ind) - 96
-        //console.log(val)
-        for(var i=1; i <= hash[key]; i++){
-            vals.add(val*i)
+        var v = hash[key];
+        for(var i = 2; i <= v; i++){
+            var sub = key.repeat(i)
+            if(!s.includes(sub)){
+                hash[key] = i-1
+                break
+            }
         }
-     }
+    }
+
     for(var a0 = 0; a0 < n; a0++){
         var x = parseInt(readLine());
         // your code goes here
-        vals.has(x) ? console.log('Yes') : console.log('No')
+        var res = inc(hash, x, s)
+        console.log(res)
     }
 
 }
