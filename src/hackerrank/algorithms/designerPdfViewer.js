0041eaaea1d6a89d59109950db36ebfcb852e0c9
Cigdem Aybar <aybarcigdem@gmail.com>
2017-08-01 15:58:22 -0400

    designerPdf solution is added

---
+++ src/hackerrank/algorithms/designerPdfViewer.js
@@ -0,0 +1,14 @@
+function main() {
+  h = readLine().split(' ');
+  h = h.map(Number);
+  var word = readLine();
+  var max = 0;
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for (var i = 0; i < word.length; i++) {
+    var ind = alphabet.indexOf(word[i]);
+    if (max <= h[ind]) {
+      max = h[ind];
+    }
+  }
+  console.log(max * word.length);
+}
