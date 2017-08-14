9bad376d16d78d27f881c79241ce876132c54295
Cigdem Aybar <aybarcigdem@gmail.com>
2017-08-14 13:33:55 -0400

    betweenTwoSets solution is added

---
+++ src/hackerrank/algorithms/betweenTwoSets.js
@@ -0,0 +1,33 @@
+function betweenTwoSets(a,b){
+  var sortedB = b.sort((x,y) => x-y);
+  var sortedA = a.sort((x,y) => y-x);
+  //sortedB[0] is the min of the b array. sortedA[0] is the max of a array
+  if (sortedB[0] < sortedA[0]){
+    return 0;
+  }
+  var y = [];
+  for(var j = sortedB[0]; j >= sortedA[0]; j--){
+    for( var i = 0; i < sortedB.length; i++){
+      if(sortedB[i] % j !== 0){
+        break;
+      } else {
+        if( i === sortedB.length -1){
+          y.push(j);
+        }
+      }
+    }
+  }
+  var x = [];
+  for(var i = 0; i < y.length; i++){
+    for(var j = 0; j < sortedA.length; j++){
+      if(y[i]% sortedA[j] !== 0){
+        break;
+      } else {
+        if( j === sortedA.length -1){
+          x.push(y[i]);
+        }
+      }
+    }
+  }
+  return x.length;
+}
